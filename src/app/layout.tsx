e5edbf20330194988621fac9560e557ed8a58639
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-09-09 12:50:49 +0900

    update seo engine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Loading from "@/components/Loading/index";
 export const metadata: Metadata = {
   title: "한평생교육",
   description:
-    "평생교육의 교육이념 실현의 선두주자 한평생교육은 언제 어디서든 교육받을 수 있는 환경과 양질의 컨텐츠를 제공 하는 것이 목표입니다. ",
+    "한평생교육은 평생교육의 교육이념 실현의 선두주자로, 언제 어디서든 교육받을 수 있는 환경과 양질의 컨텐츠를 제공하는 것이 목표입니다.",
   viewport: "width=device-width, initial-scale=1.0",
   keywords: "한평생교육, 평생교육, 교육, 컨텐츠, 환경, 지속가능한 교육",
   verification: {
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "한평생교육",
     description:
-      "평생교육의 교육이념 실현의 선두주자 한평생교육은 언제 어디서든 교육받을 수 있는 환경과 양질의 컨텐츠를 제공 하는 것이 목표입니다. ",
+      "한평생교육은 평생교육의 교육이념 실현의 선두주자로, 언제 어디서든 교육받을 수 있는 환경과 양질의 컨텐츠를 제공하는 것이 목표입니다.",
     images: "/images/og-image.png",
     siteName: "한평생교육",
     locale: "ko_KR",
